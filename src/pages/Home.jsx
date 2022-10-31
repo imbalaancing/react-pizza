cf8d532c301948abac1827de5204d853a6998d9f
nicky <nikkalaancing@gmail.com>
2022-10-31 19:35:22 +0700

    setting up a responsive layout, creating a shopping cart page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,11 @@ function Home() {
         setItems(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -32,7 +33,7 @@ function Home() {
           ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 }
 
